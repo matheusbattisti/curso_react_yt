e189f7390cca88777fddbe36a47a85294910df4d
Matheus Battisti <matheus.battisti@essentia.com.br>
2021-07-18 15:55:34 -0300

    JSX na pratica

--- src/App.js
+++ src/App.js
@@ -1,9 +1,18 @@
-import "./App.css";
-
 function App() {
+  const name = "Matheus";
+
+  function sum(a, b) {
+    return a + b;
+  }
+
+  const url = "https://via.placeholder.com/150";
+
   return (
     <div className="App">
       <p>Testando o JSX</p>
+      <p>Olá {name}</p>
+      <p>A soma é: {sum(1, 2)}</p>
+      <img src={url} alt="Minha Imagem" />
     </div>
   );
 }
