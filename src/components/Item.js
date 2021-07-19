598450daac922ef9671270e2b679e3b186ed43a0
Matheus Battisti <matheusbudkewicz@gmail.com>
2021-07-18 22:13:28 -0300

    avancando em props

--- src/components/Item.js
+++ src/components/Item.js
@@ -1,9 +1,23 @@
+import PropTypes from 'prop-types'
+
 function Item(props) {
   return (
     <>
-      <li>{props.marca}</li>
+      <li>
+        {props.marca} - {props.ano_lancamento}
+      </li>
     </>
   )
 }
 
 export default Item
+
+Item.propTypes = {
+  marca: PropTypes.string.isRequired,
+  ano_lancamento: PropTypes.number,
+}
+
+Item.defaultProps = {
+  marca: 'Faltou a marca',
+  ano_lancamento: 1,
+}
--- src/components/List.js
+++ src/components/List.js
@@ -5,9 +5,10 @@ function List() {
     <div>
       <h1>Minha Lista de Marcas:</h1>
       <ul>
-        <Item marca="Ferrari" />
-        <Item marca="Fiat" />
+        <Item marca="Ferrari" ano_lancamento={1985} />
+        <Item marca="Fiat" ano_lancamento={1964} />
         <Item marca="Honda" />
+        <Item />
       </ul>
     </div>
   )
