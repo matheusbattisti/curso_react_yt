03f2c943827c23b477cd80eec9f28130fb30a2d0
Matheus Battisti <matheusbudkewicz@gmail.com>
2021-07-19 22:44:44 -0300

    eventos em props

---
+++ src/components/Button.js
@@ -0,0 +1,5 @@
+function Button(props) {
+  return <button onClick={props.event}>{props.text}</button>
+}
+
+export default Button
--- src/components/Evento.js
+++ src/components/Evento.js
@@ -1,12 +1,20 @@
+import Button from './Button'
+
+import styles from './Evento.module.css'
+
 function Evento() {
   function meuEvento() {
     console.log('Opa, fui ativado!')
   }
 
+  function meuSegundoEvento() {
+    console.log('Segundo evento!!')
+  }
+
   return (
-    <div>
-      <p>Clique para disparar o evento:</p>
-      <button onClick={meuEvento}>Ativar!</button>
+    <div className={styles.spacing}>
+      <Button event={meuEvento} text="Primeiro Evento" />
+      <Button event={meuSegundoEvento} text="Segundo Evento" />
     </div>
   )
 }
