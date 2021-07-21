421f114228d4a8207fa3cff1d19f771984c94ec3
Matheus Battisti <matheusbudkewicz@gmail.com>
2021-07-21 12:08:54 -0300

    react icons

--- src/components/layout/Footer.js
+++ src/components/layout/Footer.js
@@ -1,5 +1,23 @@
+import { FaFacebook, FaInstagram, FaLinkedin } from 'react-icons/fa'
+import styles from './Footer.module.css'
+
 function Footer() {
-  return <footer>Rodapé</footer>
+  return (
+    <footer>
+      <ul className={styles.social_list}>
+        <li>
+          <FaFacebook />
+        </li>
+        <li>
+          <FaInstagram />
+        </li>
+        <li>
+          <FaLinkedin />
+        </li>
+      </ul>
+      <p>Nosso rodapé</p>
+    </footer>
+  )
 }
 
 export default Footer
