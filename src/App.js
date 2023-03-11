ba592fcf2d0cae0e2e9529f8e9af69fb1f5bc3de
jacksonmmartins <82293718+jacksonmmartins@users.noreply.github.com>
2023-03-11 00:12:48 -0300

    Atualização App.js
    Devido atualizações do Dom V6 - De repente pode ajudar outros alunos no desenvolvimento. Sucesso!
    Link da verificação https://stackoverflow.com/questions/63124161/attempted-import-error-switch-is-not-exported-from-react-router-dom

--- src/App.js
+++ src/App.js
@@ -1,43 +1,24 @@
-import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
-import Footer from './components/layout/Footer'
-import Navbar from './components/layout/Navbar'
-import Container from './components/layout/Container'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import Home from './components/pages/Home'
-import Projects from './components/pages/Projects'
-import NewProject from './components/pages/NewProject'
 import Company from './components/pages/Company'
 import Contact from './components/pages/Contact'
-import Project from './components/pages/Project'
-
+import NewProject from './components/pages/NewProject'
 function App() {
   return (
     <Router>
-      <Navbar />
-      <Switch>
-        <Container customClass="min-height">
-          <Route exact path="/">
-            <Home />
-          </Route>
-          <Route path="/projects">
-            <Projects />
-          </Route>
-          <Route path="/company">
-            <Company />
-          </Route>
-          <Route path="/contact">
-            <Contact />
-          </Route>
-          <Route path="/newproject">
-            <NewProject />
-          </Route>
-          <Route path="/project/:id">
-            <Project />
-          </Route>
-        </Container>
-      </Switch>
-      <Footer />
+      <ul>
+        <li>Home</li>
+        <li>Contato</li>
+      </ul>
+      <Routes>
+        <Route exact path="/" element={<Home/>}/>
+        <Route exact path="/company" element={<Company/>}/>
+        <Route exact path="/Contact" element={<Contact/>}/>
+        <Route exact path="/NewProject" element={<NewProject/>}/>
+      </Routes>
+      <p>Footer</p>
     </Router>
-  )
+  );
 }
 
 export default App
